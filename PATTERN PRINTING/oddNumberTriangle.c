dbdd2318d78b04a7a3be5990bb42578a456ac722
lendrik-kumar <sanketkamboj2006@gmail.com>
2024-08-04 00:18:40 +0530

    hello

--- PATTERN PRINTING/oddNumberTriangle.c
+++ PATTERN PRINTING/oddNumberTriangle.c
@@ -3,10 +3,11 @@ int main(){
   int i,j,l;
   printf("enter the height");
   scanf("%d",&l);
-    for(i=1;i<=l-(l/2);i++){
+    for(i=1;i<=l;i++){
+        int a=1;
       for(j=1;j<=i;j++){
-        if(j%2!=0){
-        printf("%d",j);}
+        printf("%d ",a);
+      a=a+2;
       }
       printf("\n");
     }
