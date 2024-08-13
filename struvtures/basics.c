cdef0ee0d7f0a226069a1f9d6f8f910f21cb2161
lendrik-kumar <sanketkamboj2006@gmail.com>
2024-08-13 09:46:07 +0530

    add

---
+++ struvtures/basics.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+   struct pokemon{
+     int hp;
+     int atk;
+     int speed;  
+     char tier; 
+    };
+ struct pokemon pikachu;
+  pikachu.hp=100;
+  pikachu.atk=20;
+  pikachu.speed=30;
+  pikachu.tier='A';
+  
+ printf("Pikachu's HP is %d\n",pikachu.hp);
+
+  struct pokemon charizard;
+  charizard.hp=120;
+  charizard.atk=30;
+  charizard.speed=20;
+  charizard.tier='S';
+    return 0;
+}
